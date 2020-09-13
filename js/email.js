ba413bcfa1fa8a03e8a0bd5a568c5bbc7d94e600
Thomas DoyleEngler <tpdoyle87@gmail.com>
2020-09-13 17:20:29 -0500

    adjust css and fill in projects with templates

--- js/email.js
+++ js/email.js
@@ -6,8 +6,14 @@ templateParams = {
   service: fields.service.value
 }
 userID = "tpdoyle87@gmail.com"
+
+function replaceContactForm() {
+  document.getElementById('remove').innerHTML = '<div id="ease-in" class="green hide set-height"><i class="fas fa-check large"></i><p>We have recieved your request and</p><p>we will be in touch soon</p></div>'
+  document.getElementById('ease-in').classList.add('show-success')
+}
 function sendEmail() {
   if (validateForm()) {
+    replaceContactForm()
     emailjs.send(serviceID, templateID, templateParams,userID)
   }
 }
