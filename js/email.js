506d5cc1356ea0748ed21a279bfbc941236b9cc2
Thomas DoyleEngler <tpdoyle87@gmail.com>
2020-09-13 21:24:33 -0500

    update email sender

--- js/email.js
+++ js/email.js
@@ -1,11 +1,13 @@
-templateID = "template_hhzzcmu"
-serviceID = "service_1n46doc"
-templateParams = {
-  name: fields.name.value,
-  phone: fields.phone.value,
-  service: fields.service.value
+let templateID = "template_exxxm6f"
+let serviceID = "service_6jp8yle"
+
+function templateParams() {
+  return {
+    name: fields.name.value,
+    phone: fields.phone.value,
+    service: fields.service.value
+  }
 }
-userID = "tpdoyle87@gmail.com"
 
 function replaceContactForm() {
   document.getElementById('remove').innerHTML = '<div id="ease-in" class="green hide set-height"><i class="fas fa-check large"></i><p>We have recieved your request and</p><p>we will be in touch soon</p></div>'
@@ -13,7 +15,13 @@ function replaceContactForm() {
 }
 function sendEmail() {
   if (validateForm()) {
-    replaceContactForm()
-    emailjs.send(serviceID, templateID, templateParams,userID)
+    let params = templateParams()
+    emailjs.send(serviceID, templateID, params).then(function(response) {
+      replaceContactForm()
+      console.log("Success", response)
+    }, function(error) {
+      alert("It looks like something went wrong.")
+      console.log("Failed", error)
+    })
   }
 }
