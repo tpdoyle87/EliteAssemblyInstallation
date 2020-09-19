ac0771c1615847ec6b47b5f8d2c9e6b0a467a148
Thomas DoyleEngler <tpdoyle87@gmail.com>
2020-09-19 17:18:09 -0500

    add sending spinner to replace contact form

--- js/email.js
+++ js/email.js
@@ -9,13 +9,20 @@ function templateParams() {
   }
 }
 
+function loadingSpinner() {
+  document.getElementById('remove').innerHTML = '<div id="spinner" class="set-height cen-spin"><i class="fas fa-spinner medium spin"></i><p>Sending...</p></div>'
+  // document.getElementById('spinner').classList.add('show-success');
+}
+
 function replaceContactForm() {
   document.getElementById('remove').innerHTML = '<div id="ease-in" class="green hide set-height"><i class="fas fa-check large"></i><p>We have recieved your request and</p><p>we will be in touch soon</p></div>'
   document.getElementById('ease-in').classList.add('show-success')
 }
+
 function sendEmail() {
   if (validateForm()) {
     let params = templateParams()
+    loadingSpinner()
     emailjs.send(serviceID, templateID, params).then(function(response) {
       replaceContactForm()
       console.log("Success", response)
